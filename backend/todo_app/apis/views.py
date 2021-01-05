cc63b0872bcc7cae63c09f17e6d5e5ba4e34ccff
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-01-05 08:43:53 +0700

    api views

--- backend/todo_app/apis/views.py
+++ backend/todo_app/apis/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from todos import models
+from .serializers import TodosSerializer
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = models.object.all()
+    serializer_class = TodosSerializer
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.object.all()
+    serialzer_class = TodosSerializer
